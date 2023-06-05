d075d2b35b1b66fb0c5780f0d1f006646f71750a
AKAAka1450 <58277892+AKAAka1450@users.noreply.github.com>
2023-06-05 17:20:50 +0800

    maybe finish

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ const endpoint = configGet.get("ENDPOINT");
 const apiKey = configGet.get("FORM_RECOGNIZER_API_KEY");
 
 const imgFolder = 'saved_images/';
-const imgPath = './public/' + imgFolder;
-fs.mkdir(imgFolder, {recursive: true}, (err) => {
+const imgFolderPath = './public/' + imgFolder;
+fs.mkdir(imgFolderPath, {recursive: true}, (err) => {
 	if ( err ) throw err;
 });
 
@@ -59,15 +59,7 @@ async function writeToFile(content, filePath) {
     });
 }
 
-async function readdirPromise(folder) {
-	return new Promise((resolve, reject) => {
-		fs.readdir(folder, (err, filenames) => {
-			if ( err ) reject(err);
-			else resolve(filenames);
-		});
-	});
-}
-
+/*
 async function findJPG(folder, target) {
 	if ( target.substring(target.length-4) !== '.jpg' ) target += '.jpg';
 	return new Promise((resolve, reject) => {
@@ -88,6 +80,7 @@ async function findJPG(folder, target) {
 		});
 	});
 }
+*/
 
 
 // 呼叫 Azure Form Recognizer
@@ -128,6 +121,7 @@ async function performFormRecognition(filePath) {
 			}
 		}
 		return {
+			name: filePath.substring(filePath.lastIndexOf("/")+1),
 			pageNumber: page.pageNumber,
 			unit: page.unit,
 			width: page.width,
@@ -140,10 +134,33 @@ async function performFormRecognition(filePath) {
 	return {};
 }
 
+function lcs(sA, sB) {
+	let dp = Array(2);
+	for ( var i=0; i<2; i++ ) dp[i] = Array(sB.length+1);
+	for ( var j=0; j<sB.length+1; j++ ) dp[0][j] = 0;
+
+	for ( var i=1; i<sA.length+1; i++ ) {
+		dp[i%2][0] = 0;
+		for ( var j=1; j<sB.length+1; j++ ) {
+			if ( sA[i-1] === sB[j-1] ) {
+				dp[i%2][j] = dp[(i-1)%2][j-1] + 1;
+			} else {
+				dp[i%2][j] = Math.max(dp[(i-1)%2][j], dp[i%2][j-1]);
+			}
+		}
+	}
+	return dp[sA.length%2][sB.length]
+}
+
 function replyHelp(event) {
 	client.replyMessage(event.replyToken, {
 		type: 'text',
-		text: 'HELP MESSAGE'
+		text: '使用說明：\n-\n' +
+				'ls\n列出現存檔案名稱\n-\n' +
+				'mv file1 file2\n將 file1 改名為 file2\n-\n' +
+				'rm file1\n刪除 file1\n-\n' +
+				'show file1\n顯示 file1 圖片\n-\n' +
+				'上傳圖片：新增圖片至資料庫，並與舊有資料比對。'
 	});
 }
 
@@ -151,38 +168,41 @@ function replyHelp(event) {
 async function handleEvent(event) {
     if (event.message.type === 'image') {
         const messageContent = await client.getMessageContent(event.message.id);
-        const filePath = `${imgPath}/${event.message.id}.jpg`;
 
-        // 儲存圖片到本地
-        const writableStream = fs.createWriteStream(filePath);
-        await new Promise((resolve, reject) => {
-            messageContent.pipe(writableStream);
-            writableStream.on('finish', resolve);
-            writableStream.on('error', reject);
-        });
+		// 儲存圖片到本地
+		const filename = `${event.message.id}.jpg`;
+		const filepath = `${imgFolderPath}/${filename}`
+		const writableStream = fs.createWriteStream(filepath);
+		await new Promise((resolve, reject) => {
+			messageContent.pipe(writableStream);
+			writableStream.on('finish', resolve);
+			writableStream.on('error', reject);
+		});
 
-        // client.replyMessage只能回傳一次訊息
 		// 讀取現有的 output.json 內容
-		let exJSON = {};
+		let pastJSON = {};
 		try {
 			const existingData = fs.readFileSync(outputFilePathJSON, 'utf8')
 			if (existingData) {
-				exJSON = JSON.parse(existingData);
+				pastJSON = JSON.parse(existingData);
 			} else {
-				exJSON = { pages: [] };
+				pastJSON = { pages: [] };
 			}
 		} catch ( { name, msg } ) {
 			fs.closeSync(fs.openSync(outputFilePathJSON, 'w'));
-			exJSON = { pages:[] };
+			pastJSON = { pages:[] };
 		}
-		
 
         // 圖片轉文字
-        performFormRecognition(filePath)
+        performFormRecognition(filepath)
 		.then( (outputJSON) => {
-			for ( const page of exJSON.pages ) {
+			for ( const page of pastJSON.pages ) {
 				if ( page.content === outputJSON.content ) {
 					console.log("Content already exist.");
+					fs.rm(filepath, err => {
+						if ( err ) throw err;
+						console.log(`removed (duplicated) file ${filepath}`)
+					});
 					client.replyMessage(event.replyToken, {
 						type: 'text',
 						text: '已上傳過（內容）相同的檔案。'
@@ -190,9 +210,84 @@ async function handleEvent(event) {
 					return;
 				}
 			}
+
+			let replyMsg = [];
+
+			replyMsg.push({
+				type: 'text',
+				text: `已儲存圖片為 ${filename}。`
+			});
+
+			// 將此文檔與過去其他文檔比對
+			if ( pastJSON.pages.length < 1 ) {
+				replyMsg.push({
+					type: 'text',
+					text: '無歷史文檔可供比對。'
+				});
+				client.replyMessage(event.replyToken, replyMsg);
+				// 儲存至json
+				pastJSON.pages.push(outputJSON);
+				writeToFile(JSON.stringify(pastJSON), outputFilePathJSON);
+				return;
+			}
+
+			const delims = /[！？。!?\n]+/;
+			const MIN_LEN = 7;
+			const compareResults = [];
 			if ( outputJSON ) {
-				exJSON.pages.push(outputJSON);
-				writeToFile(JSON.stringify(exJSON), outputFilePathJSON);
+				const newSentences = outputJSON.content.split(delims);
+				for ( const page of pastJSON.pages ) {
+					const pastSentences = page.content.split(delims);
+							
+					for ( const newSentence of newSentences ) {
+						if ( newSentence.length < MIN_LEN ) continue;
+						for ( const pastSentence of pastSentences ) {
+							if ( pastSentence.length < MIN_LEN ) continue;
+							const rat = lcs(newSentence, pastSentence) / Math.max(newSentence.length, pastSentence.length);
+							if ( rat > 0.8 ) compareResults.push({
+								pastName: page.name,
+								newS: newSentence,
+								pastS: pastSentence,
+								rat: rat
+							});
+						}
+					}
+				}
+			}
+
+			if ( compareResults.length === 0 ) {
+				replyMsg.push({
+					type: 'text',
+					text: `在 ${pastJSON.pages.length} 筆歷史文檔中沒有找到相似的句子。`
+				});
+			} else {
+				replyMsg.push({
+					type: 'text',
+					text: `與歷史文檔比對共找到 ${compareResults.length} 組相似的句子。其中最相似的 ${Math.min(3, compareResults.length)} 組為：`
+				});
+
+				compareResults.sort( (a, b) => {
+					return a[rat] > b[rat];
+				});
+
+				for ( var i=0; i<Math.min(3, compareResults.length); i++ ) {
+					const res = compareResults[i];
+					replyMsg.push({
+						type: 'text',
+						text: `此文檔：\n${res.newS}\n${res.pastName}：\n-\n${res.pastS}\n-\n相似度：${res.rat}`
+					});
+				}
+			}
+
+			client.replyMessage(event.replyToken, replyMsg);
+
+			pastJSON.pages.push(outputJSON);
+			writeToFile(JSON.stringify(pastJSON), outputFilePathJSON);
+
+			/*
+			if ( outputJSON ) {
+				pastJSON.pages.push(outputJSON);
+				writeToFile(JSON.stringify(pastJSON), outputFilePathJSON);
 				console.log("File has been written successfully.");
 
 				exec('python3 compare.py', (error, stdout, stderr) => {
@@ -206,6 +301,7 @@ async function handleEvent(event) {
 					});
 				});
 			}
+			*/
 		});
 
     } else if (event.message.type === 'text') {
@@ -218,9 +314,13 @@ async function handleEvent(event) {
 		switch( words[0] ) {
 			case 'ls':
 				console.log('Get command \'ls\'');
-				fs.readdir(imgPath, (err, files) => {
-					if ( err ) {
-						console.log(err);
+				fs.readdir(imgFolderPath, (err, files) => {
+					if ( err  || files.length === 0) {
+						client.replyMessage(event.replyToken, {
+							type: 'text',
+							text: '暫無檔案可供讀取'
+						});
+						console.error(err);
 						return;
 					}
 					client.replyMessage(event.replyToken, {
@@ -237,9 +337,9 @@ async function handleEvent(event) {
 					break;
 				}
 
-				findJPG(imgPath, words[1])
-				.then((file) => {
-					const replyImgPath = base_url + '/' + imgFolder + '/' + file;
+				if ( words[1].substring(words[1].length-4) !== '.jpg' ) words[1] += '.jpg';
+				if ( fs.existsSync(imgFolderPath+words[1])) {
+					const replyImgPath = base_url + '/' + imgFolder + '/' + words[1];
 					return client.replyMessage(event.replyToken, [
 					{
 						type: 'text',
@@ -251,13 +351,12 @@ async function handleEvent(event) {
 						'previewImageUrl': replyImgPath
 					}
 					]);
-				})
-				.catch((err) => {
+				} else {
 					client.replyMessage(event.replyToken, {
 						type: 'text',
-						text: '找不到此名稱的圖片: ' + words[1]
+						text: `失敗，找不到名稱為 \'${words[1]}\' 的圖片。`
 					});
-				});
+				}
 				break;
 
 			case 'mv':
@@ -268,12 +367,20 @@ async function handleEvent(event) {
 				}
 
 				if ( words[1].substring(words[1].length-4) !== '.jpg' ) words[1] += '.jpg';
-				if ( fs.existsSync(imgPath+words[1])) {
+				if ( fs.existsSync(imgFolderPath+words[1])) {
 					if ( words[2].substring(words[2].length-4) !== '.jpg' ) words[2] += '.jpg';
-					if ( fs.existsSync(imgPath+words[2]) ) {
+					if ( fs.existsSync(imgFolderPath+words[2]) ) {
 						replyText = `失敗，已存在名稱為 \'${words[2]}\' 的圖片。`;
 					} else {
-						fs.rename(imgPath+words[1], imgPath+words[2], err => {
+						//改output.json中的名字
+						let pastJSON = JSON.parse(fs.readFileSync(outputFilePathJSON, 'utf8'));
+						for ( const page of pastJSON.pages ) {
+							if ( page.name === words[1] ) page.name = words[2];
+							break;
+						}
+						writeToFile(JSON.stringify(pastJSON), outputFilePathJSON);
+						//改saved_image中的名字
+						fs.rename(imgFolderPath+words[1], imgFolderPath+words[2], err => {
 							if ( err ) throw err;
 							console.log('file renamed!');
 						});
@@ -296,8 +403,18 @@ async function handleEvent(event) {
 				}
 
 				if ( words[1].substring(words[1].length-4) !== '.jpg' ) words[1] += '.jpg';
-				if ( fs.existsSync(imgPath+words[1]) ) {
-					fs.rm(imgPath+words[1], err => {
+				if ( fs.existsSync(imgFolderPath+words[1]) ) {
+					//刪除 output.json 中的資料
+					let pastJSON = JSON.parse(fs.readFileSync(outputFilePathJSON, 'utf8'));
+					for ( const page of pastJSON.pages ) {
+						if ( page.name === words[1] ) pastJSON.pages = pastJSON.pages.filter( (ele) => {
+							return ele != page;
+						});
+						break;
+					}
+					writeToFile(JSON.stringify(pastJSON), outputFilePathJSON);
+					// 刪除 saved_image 中的圖片
+					fs.rm(imgFolderPath+words[1], err => {
 						if ( err ) throw err;
 						console.log('file removed!');
 					})
